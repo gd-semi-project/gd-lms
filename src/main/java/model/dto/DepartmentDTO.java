e7fe7c26cd24a10d7f58ffca72b15cc0510a5b2e
shinjunggean <wjdrmsdn6825@gmail.com>
2026-01-15 12:15:26 +0900

    departmentdto, studentdto생성

---
+++ src/main/java/model/dto/DepartmentDTO.java
@@ -0,0 +1,6 @@
+package model.dto;
+
+public class DepartmentDTO {
+	private int department_id;
+	private String department_name;	// 부서/전공
+}
---
+++ src/main/java/model/enumtype/StudentStatus.java
@@ -0,0 +1,7 @@
+package model.enumtype;
+
+public enum StudentStatus {
+	ENROLLED,	// 재학
+	LEAVE,		// 휴학
+	GRADUATED	// 졸업
+}
