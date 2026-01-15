440743a90c438e34ab46b2ff62f3664c15954e6a
shinjunggean <wjdrmsdn6825@gmail.com>
2026-01-15 15:39:34 +0900

    StudentDAO작성

--- src/main/java/model/dto/StudentsDTO.java
+++ src/main/java/model/dto/StudentsDTO.java
@@ -3,20 +3,22 @@ package model.dto;
 import java.time.LocalDateTime;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import model.enumtype.StudentStatus;
 import model.enumtype.StudentType;
 
 @Data
+@NoArgsConstructor
 public class StudentsDTO {
-	private int student_id;
-	private int department_id;
-	private int user_id;
+	private int studentId;
+	private int departmentId;
+	private int userId;
 	
-	private Integer student_number;	// 학번
-	private int student_grade;	// 학년
+	private Integer studentNumber;	// 학번
+	private int studenGrade;	// 학년
 	private StudentType status;	// 학부냐 대학원이냐
-	private StudentStatus student_status;	// 재학 상태
-	private LocalDateTime enroll_date;	// 입학일자
-	private LocalDateTime end_date;		// 졸업일자
-	private String tuition_account;		// 등록금 계좌
+	private StudentStatus studentStatus;	// 재학 상태
+	private LocalDateTime enrollDate;	// 입학일자
+	private LocalDateTime endDate;		// 졸업일자
+	private String tuitionAccount;		// 등록금 계좌
 }
