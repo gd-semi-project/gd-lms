3a155b2318f5855df117960f4cf0d6171a17cfe5
horang100 <goheekwon9991@gmail.com>
2026-01-15 15:43:30 +0900

    로그인기능 검증완료

--- src/main/java/model/dao/UserDAO.java
+++ src/main/java/model/dao/UserDAO.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 
 import database.DBConnection;
 import model.dto.UserDTO;
+import model.enumtype.Gender;
 import model.enumtype.Role;
 import model.enumtype.Status;
 import model.enumtype.YesOrNo;
@@ -22,7 +23,7 @@ public class UserDAO {
 	}
 	
 	public UserDTO SelectUsersById(String id) {
-		String sql = "SELECT * FROM users WHERE id = ?";
+		String sql = "SELECT * FROM users WHERE login_id = ?";
 		UserDTO userDTO = new UserDTO();
 		try (Connection conn = DBConnection.getConnection()){
 			PreparedStatement pstmt = conn.prepareStatement(sql);
@@ -33,9 +34,14 @@ public class UserDAO {
 				userDTO.setLogin_id(rs.getString("login_id"));
 				userDTO.setPassword(rs.getString("password_hash"));
 				userDTO.setName(rs.getString("name"));
+				
+				String genderStr = rs.getString("gender"); 
+				userDTO.setGender(Gender.valueOf(genderStr));
+				
 				userDTO.setBirth_date(rs.getDate("birth_date").toLocalDate());
 				userDTO.setEmail(rs.getString("email"));
 				userDTO.setPhone(rs.getString("phone"));
+				userDTO.setAddress(rs.getString("address"));
 				
 				String roleStr = rs.getString("role");
 				userDTO.setRole(Role.valueOf(roleStr));
@@ -43,16 +49,37 @@ public class UserDAO {
 				String statusStr = rs.getString("status"); 
 				userDTO.setStatus(Status.valueOf(statusStr));
 				
-				String mustChangPwStr = rs.getString("must_change_pw"); 
-				userDTO.setMustChangePw(YesOrNo.valueOf(mustChangPwStr));
+				userDTO.setMustChangePw(rs.getBoolean("must_change_pw"));
 				
 				userDTO.setCreated_at(rs.getTimestamp("created_at").toLocalDateTime());
 				userDTO.setUpdated_at(rs.getTimestamp("updated_at").toLocalDateTime());
 			}
 			return userDTO;
 		} catch (SQLException | ClassNotFoundException e) {
-			// 예외처리 구문 작성 필요
+			// TODO: 예외처리 구문 작성 필요
+			System.out.println("UserDAO selectuserbyid" + e.getMessage());
 		}
 		return null;
+	}
+	
+	public void InsertUser(UserDTO userDTO) {
+		String sql = "INSERT INTO users (login_id, password_hash, name, birth_date, email, phone, role)"
+				+ " VALUES (?,?,?,?,?,?,?)";
+				
+		try (Connection conn = DBConnection.getConnection()){
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userDTO.getLogin_id());
+			pstmt.setString(2, userDTO.getPassword());
+			pstmt.setString(3, userDTO.getName());
+			pstmt.setObject(4, userDTO.getBirth_date());
+			pstmt.setString(5, userDTO.getEmail());
+			pstmt.setString(6, userDTO.getPhone());
+			pstmt.setString(7, userDTO.getRole().toString());
+			pstmt.executeUpdate();
+		} catch (SQLException | ClassNotFoundException e) {
+			// TODO: 예외처리 구문 작성 필요
+			System.out.println(e.getMessage() + "111111");
+		}
+		
 	}
 }
