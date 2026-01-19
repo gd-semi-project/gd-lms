92cda94cd493ff7f22cb9c4b746b7e18a8175ecf
shinjunggean <wjdrmsdn6825@gmail.com>
2026-01-19 10:10:58 +0900

    test1

--- src/main/java/model/dto/ScoreDTO.java
+++ src/main/java/model/dto/ScoreDTO.java
@@ -10,9 +10,9 @@ import lombok.NoArgsConstructor;
 public class ScoreDTO {
 	private Long gradeId;
 	
-	private int enrollmentId; // enroll FK
-	private int lectureId;	// lecture FK
-	private int usersId;	// user FK
+	private long enrollmentId; // enroll FK
+	private long lectureId;	// lecture FK
+	private long usersId;	// user FK
 	
 	// 점수 입력전에 값은 NULL이여야 하기 때문 int대신 Integer사용(int는 NULL불가)
 	private Integer attendanceScore;	// 출석 점수
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
 <%
-  request.getRequestDispatcher("/WEB-INF/views/login/login.jsp")
+  request.getRequestDispatcher("/WEB-INF/test.jsp")
          .forward(request, response);
 %>
 
