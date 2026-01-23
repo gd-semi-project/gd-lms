aafa7d9b5678b3c7e6db4e0ba7b0c679498019c1
김현호 <kimhyenho41@gmail.com>
2026-01-23 15:58:38 +0900

    up

--- src/main/java/model/dao/EnrollmentDAO.java
+++ src/main/java/model/dao/EnrollmentDAO.java
@@ -104,53 +104,7 @@ public class EnrollmentDAO {
 
 			return list;
 		}
-  // 수강생 목록 조회 : 지윤
-//   public List<LectureStudentDTO> findStudentsByLectureId(long lectureId) {
-//
-//		String sql = """
-//				    SELECT
-//				        s.student_id,
-//				        u.user_id,
-//				        u.name,
-//				        s.student_number,
-//				        s.student_grade,
-//				        e.status,
-//				        e.applied_at
-//				    FROM enrollment e
-//				    JOIN student s ON e.student_id = s.student_id
-//				    JOIN users u ON s.user_id = u.user_id
-//				    WHERE e.lecture_id = ?
-//				    ORDER BY s.student_number
-//				""";
-//
-//		List<LectureStudentDTO> list = new ArrayList<>();
-//
-//		try (Connection conn = DBConnection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
-//
-//			pstmt.setLong(1, lectureId);
-//
-//			try (ResultSet rs = pstmt.executeQuery()) {
-//				while (rs.next()) {
-//					LectureStudentDTO dto = new LectureStudentDTO();
-//					dto.setStudentId(rs.getLong("student_id"));
-//					dto.setUserId(rs.getLong("user_id"));
-//					dto.setStudentName(rs.getString("name"));
-//					dto.setStudentNumber(rs.getInt("student_number"));
-//					dto.setStudenGrade(rs.getInt("student_grade"));
-//					dto.setEnrollmentStatus(EnrollmentStatus.valueOf(rs.getString("status")));
-//					dto.setAppliedAt(rs.getTimestamp("applied_at").toLocalDateTime());
-//
-//					list.add(dto);
-//				}
-//			}
-//		} catch (Exception e) {
-//			throw new RuntimeException("수강생 목록 조회 실패", e);
-//		}
-//
-//		return list;
-//	}
 
-//
    // 학생ID로 특정강의ID 이수 확인 메소드
    public boolean isStudentEnrolled(Connection conn, long userId, long lectureId) throws SQLException {
 	    String sql = """
